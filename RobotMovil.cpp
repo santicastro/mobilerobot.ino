1b8b335eb7b96aa0c7bd7baadcc2261ea9d594f0
Santiago Castro Castrillon <scastro@trileucosolutions.com>
2013-11-29 20:34:51 +0100

    Bug fix

--- RobotMovil.cpp
+++ RobotMovil.cpp
@@ -2,22 +2,24 @@
 #include "RobotMovil.h"
 #include "NewPing.h"
 
-//// INIT ////
+//// PINS ////
 // Motion
-int LEFT_SPEED_PIN = 5;
-int LEFT_FORWARD_PIN = 7;
-int LEFT_REVERSE_PIN = 8;
-int RIGHT_SPEED_PIN = 6;
-int RIGHT_FORWARD_PIN = 2;
-int RIGHT_REVERSE_PIN = 4;
+#define LEFT_SPEED_PIN 5
+#define LEFT_FORWARD_PIN 7
+#define LEFT_REVERSE_PIN 8
+#define RIGHT_SPEED_PIN 6
+#define RIGHT_FORWARD_PIN 2
+#define RIGHT_REVERSE_PIN 4
+
+#define LEFT_SENSOR_PIN A0
+#define CENTER_SENSOR_PIN A4
+#define RIGHT_SENSOR_PIN A5
+
 
 // Line follow sensors
 const int TOTAL_SENSORS = 3;
-const int MAX_SENSOR_READS = 3;
-const int SENSOR_RANGE = 50;
-int LEFT_SENSOR_PIN = A0;
-int CENTER_SENSOR_PIN = A5;
-int RIGHT_SENSOR_PIN = A4;
+const int MAX_SENSOR_READS = 1;
+const int SENSOR_RANGE = 200;
 struct Sensor_data{
 	int pin;
 	int background_value;
@@ -104,7 +106,7 @@ void RobotMovil::autoCalibrateLineSensors(){
 		for(int j=0; j<TOTAL_SENSORS; j++){
 			sensor_states[j].background_value += analogRead(sensor_states[j].pin);
 		}
-		delay(100);
+		delay(30);
 	}
 	brake(1000);
 	sensor_states[LEFT_SENSOR].background_value /= MAXREADS;
@@ -112,22 +114,20 @@ void RobotMovil::autoCalibrateLineSensors(){
 	sensor_states[RIGHT_SENSOR].background_value /= MAXREADS;
 }
 
-bool RobotMovil::detectLine(int sensor){
-	int pin = sensor_states[sensor].pin;
-	int value = 0;
-	for(int i=0; i<MAX_SENSOR_READS;){
+bool RobotMovil::detectLine(int which){
+	int pin = sensor_states[which].pin;
+	int value = analogRead(pin);
+	for(int i=1; i<MAX_SENSOR_READS;i++){
+		delay(1);
 		value += analogRead(pin);
-		if(i++<MAX_SENSOR_READS){
-			delay(2);
-		}
 	}
 	value /= MAX_SENSOR_READS;
-	int difference = sensor_states[sensor].background_value - value;
+	int difference = abs(sensor_states[which].background_value - value);
 	if(difference > SENSOR_RANGE){
 		return true;
 	}else{
 		//reajusts the background color
-		sensor_states[sensor].background_value = sensor_states[sensor].background_value*0.9 + value*0.1;
+		sensor_states[which].background_value = sensor_states[which].background_value*0.9 + value*0.1;
 		return false;
 	}
 }
--- RobotMovil.h
+++ RobotMovil.h
@@ -4,6 +4,17 @@
 #define CENTER_SENSOR 1
 #define RIGHT_SENSOR 2
 
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include <Arduino.h>
+#else
+	#include <WProgram.h>
+	#include <pins_arduino.h>
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
 class RobotMovil {
   public:
     RobotMovil();
